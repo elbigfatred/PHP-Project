8c6700b5cd9449a5ffefa2236e81187b29baf834
NoahEvansRitcey <145677096+NoahEvansRitcey@users.noreply.github.com>
2024-10-21 19:30:31 -0300

    Update DatabaseConstants.php

--- Database/Classes/DatabaseConstants.php
+++ Database/Classes/DatabaseConstants.php
@@ -1,8 +1,8 @@
 <?php
 class DatabaseConstants
 {
-    public static $MYSQL_CONNECTION_STRING = "mysql:host=localhost;dbname=bowling_tournament";
+    public static $MYSQL_CONNECTION_STRING = "mysql:host=localhost;dbname=bowlingtournament";
     public static $MYSQL_CONNECTION_NO_DB = "mysql:host=localhost";
-    public static $MYSQL_USERNAME = "elbigfatred";
-    public static $MYSQL_PASSWORD = "1234";
+    public static $MYSQL_USERNAME = "noahr";
+    public static $MYSQL_PASSWORD = "noahr";
 }
