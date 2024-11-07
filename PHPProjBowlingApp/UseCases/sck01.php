bc918405232013be2c6a174e46c899bedf18b726
NoahEvansRitcey <145677096+NoahEvansRitcey@users.noreply.github.com>
2024-11-07 09:44:21 -0400

    Update sck01.php

--- PHPProjBowlingApp/UseCases/sck01.php
+++ PHPProjBowlingApp/UseCases/sck01.php
@@ -1,38 +1,15 @@
 <?php
-// UseCases/sck01.php
-
-// Set JSON header
-header('Content-Type: application/json');
 
 require_once dirname(__DIR__, 1) . '/Database/Classes/ConnectionManager.php';
 require_once dirname(__DIR__, 1) . '/Database/Classes/DAO.php';
 require_once dirname(__DIR__, 1) . '/Database/Classes/DatabaseConstants.php';
 require_once dirname(__DIR__, 1) . '/Database/Classes/scoreKeeperServices.php';
+// Create a new ConnectionManager instance using database credentials from DatabaseConstants
+$cm = new ConnectionManager(DatabaseConstants::$MYSQL_CONNECTION_STRING, DatabaseConstants::$MYSQL_USERNAME, DatabaseConstants::$MYSQL_PASSWORD);
+// Create a databaseAccessorObject to interact with the Database.
+$dao = new DAO($cm->getConnection());
+$scoreKeeper = new ScoreKeeperServices($dao);
 
-try {
-    // Get roundId from query parameter
-    $roundId = $_GET['roundId'] ?? 'QUAL';
+$qualGames = $scoreKeeper->getGamesWithTeamInfo('QUAL');
 
-    // Create service instances
-    $cm = new ConnectionManager(
-        DatabaseConstants::$MYSQL_CONNECTION_STRING, 
-        DatabaseConstants::$MYSQL_USERNAME, 
-        DatabaseConstants::$MYSQL_PASSWORD
-    );
-    
-    $dao = new DAO($cm->getConnection());
-    $scoreKeeper = new ScoreKeeperServices($dao);
-
-    // Get games
-    $games = $scoreKeeper->getGamesWithTeamInfo($roundId);
-
-    // Return JSON response
-    echo json_encode($games);
-
-} catch (Exception $e) {
-    http_response_code(500);
-    echo json_encode([
-        'error' => true,
-        'message' => $e->getMessage()
-    ]);
-}
+echo json_encode($qualGames);
